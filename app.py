5f416d5693d525c966dd07162f0f4b2441ea674a
taminur <taminur@live.com>
2023-07-19 23:46:54 +0600

    Security layer is added to avoid hacking

--- app.py
+++ app.py
@@ -3,15 +3,23 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 REGISTRANTS = {} # this variable will collect all the info those who are registered
-
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate frisbee"
+    ]
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports = SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
+    if not name: # this if added to avoid getting hacked
+        return render_template("failure.html")
     sport = request.form.get("sport")
+    if sport not in SPORTS: # this if added to avoid getting hacked
+        return render_template("failure.html")
     REGISTRANTS[name] = sport
     return render_template("success.html")
 
