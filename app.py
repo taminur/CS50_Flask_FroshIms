418b0e507045817354661eef11ad1a5e976464e1
taminur <taminur@live.com>
2023-07-19 23:24:05 +0600

    Global variable REGISTRANTS of type dict is used to store all registrant's info, kind of database

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+REGISTRANTS = {} # this variable will collect all the info those who are registered
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    REGISTRANTS[name] = sport
     return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
