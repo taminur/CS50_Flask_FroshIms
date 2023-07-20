9b2f660ed162615426a163ef31e128294e9a3ad6
taminur <taminur@live.com>
2023-07-20 20:18:05 +0600

    Registrants feature added, deregistraroption added but there is security issue anyone can deregister anyone

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-REGISTRANTS = {} # this variable will collect all the info those who are registered
+con = sqlite3.connect("froshims.db", check_same_thread=False) # creating a connection object of the database
+cur = con.cursor() # creating a cursor
+
+cur.execute("CREATE TABLE IF NOT EXISTS registrants(id INTEGER PRIMARY KEY, \
+            name TEXT NOT NULL, sport TEXT NOT NULL)")
+
 SPORTS = [
     "Basketball",
     "Soccer",
@@ -14,15 +20,34 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    
     name = request.form.get("name")
-    if not name: # this if added to avoid getting hacked
-        return render_template("failure.html")
     sport = request.form.get("sport")
-    if sport not in SPORTS: # this if added to avoid getting hacked
+    
+    # validate submission
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
-    REGISTRANTS[name] = sport
-    return render_template("success.html")
+    
+    # Remember registrants
+    #cur.executemany is used for example: to store multiple rows of data 
+    # cur.execute("INSERT INTO registrants VALUES(?, ?)", (name, sport))
+    cur.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", (name, sport))
+    con.commit()
+
+    # confirm registration
+    return redirect("/registrants") # redirect() redirects anywhere on the internet
 
 @app.route("/registrants")
 def registrants():
+    REGISTRANTS = (cur.execute("SELECT * FROM registrants")).fetchall()
     return render_template("registrants.html", registrants=REGISTRANTS)
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+
+    # Forget registrant
+    id = request.form.get("id")
+    if id:
+        cur.execute("DELETE FROM registrants WHERE id = ?", [id])
+        con.commit()
+    return redirect("/registrants")
